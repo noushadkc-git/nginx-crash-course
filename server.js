2388931c30ee6be204eb4876f972dcc1e0d2a5bd
noushadkc-git <to_noushad@yahoo.co.in>
2024-12-31 14:14:41 +0400

    Final 31-12-2024

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const appName = process.env.APP_NAME
+const appName = process.env.APP_NAME //To access environment variable APP_NAME from docker-compose.yaml
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-    console.log(`Request served by ${appName}`);
+    console.log(`Request served by ${appName}`);      //appName is assigned as environment variable APP_NAME from docker-compose.yaml
 });
 
 app.listen(port, () => {
-    console.log(`${appName} is listening on port ${port}`);
+    console.log(`${appName} is listening on port ${port}`);   //appName is assigned as environment variable APP_NAME from docker-compose.yaml
 });
